28c1cedbf520bac5411720023762b23de9f37657
Vladimir <v.v.levikov@gmail.com>
2020-02-11 00:41:45 +0300

    Домашнее задание по поиску объектов

---
+++ finding_elements/locators/__init__.py
@@ -0,0 +1,5 @@
+from .ProductPage import ProductPage
+from .MainPage import MainPage
+from .SearchResultPage import SearchResultPage
+from .CategoryPage import  CategoryPage
+from .AdminAuthPage import AdminAuthPage
